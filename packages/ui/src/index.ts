a310965ab6504cd88179e475ed8fcf8167a8e35c
vitorgabriel27 <vitor.alves@geowellex.com>
2026-01-18 23:31:46 -0300

    feat: refactor UI components to use @multiproduct/ui package and remove legacy components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,13 @@ export * from './components/Header.js';
 export * from './components/Sidebar.js';
 export * from './layout/AppShell.js';
 export { RootLayout } from './layout/RootLayout.js';
+
+// shadcn/ui components
+export * from './components/ui/avatar.js';
+export * from './components/ui/badge.js';
+export * from './components/ui/button.js';
+export * from './components/ui/card.js';
+export * from './components/ui/separator.js';
+
+// utils
+export { cn } from './lib/utils.js';
